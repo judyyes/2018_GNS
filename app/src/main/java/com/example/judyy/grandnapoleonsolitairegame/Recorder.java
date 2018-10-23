f870e1bda71d880b33569bb3fae6eea4dd5a3c2f
Zhuo Cheng Du <agains1595@gmail.com>
2018-10-23 11:51:44 -0400

    create recorder class

---
+++ app/src/main/java/com/example/judyy/grandnapoleonsolitairegame/Recorder.java
@@ -0,0 +1,26 @@
+package com.example.judyy.grandnapoleonsolitairegame;
+
+import java.util.Stack;
+
+public class Recorder {
+    private GameActivity mGameActivity;
+    private Stack<OneStep> mRecord;
+
+    class OneStep{
+
+    }
+
+    public Recorder(GameActivity gameActivity){
+        mGameActivity = gameActivity;
+    }
+
+    public void undoOneStep(){
+
+    }
+
+    public void record(){
+
+    }
+
+
+}
